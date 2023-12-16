9e2979629b232ec2a4b330e98a8cd90b04899f7a
kckmdev <kamalichchou0@gmail.com>
2023-12-16 16:15:45 +0100

    🐛  texte useless mais toi même tu sais

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,14 +31,16 @@
                     Forma
                 </h2>
                 <p class="mt-2 text-center text-sm text-gray-600">
-                    Bienvenue sur Forma, le site de gestion des formations  pour les salariés et bénévoles des associations affiliées à la maison régionale des sports
-de Lorraine..
+                    Bienvenue sur Forma, le site de gestion des formations pour les salariés et bénévoles des
+                    associations affiliées à la maison régionale des sports
+                    de Lorraine..
                 </p>
             </div>
 
             <div class="mt-8 space-y-6">
                 <div class="rounded-md shadow-sm -space-y-px">
-                    <a href="/login" class="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 md:py-2 md:text-lg md:px-10">
+                    <a href="/login"
+                        class="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 md:py-2 md:text-lg md:px-10">
                         Connexion
                     </a>
                 </div>
@@ -51,7 +53,9 @@ de Lorraine..
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Gestion et Leadership</div>
                 <p class="text-gray-700 text-base">
-                Développez vos compétences en gestion et leadership avec nos formations spécialisées. Apprenez à conduire des réunions efficacement, à communiquer avec la presse, et à gérer les responsabilités des dirigeants dans le sport.
+                    Développez vos compétences en gestion et leadership avec nos formations spécialisées. Apprenez à
+                    conduire des réunions efficacement, à communiquer avec la presse, et à gérer les responsabilités des
+                    dirigeants dans le sport.
                 </p>
             </div>
             <div class="px-6 pt-4 pb-2">
@@ -67,7 +71,8 @@ de Lorraine..
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Informatique et Design</div>
                 <p class="text-gray-700 text-base">
-                Maîtrisez les outils informatiques et de design les plus demandés. Que vous soyez débutant ou avancé, nos formations en Outlook, PowerPoint et Photoshop vous aideront à exceller.
+                    Maîtrisez les outils informatiques et de design les plus demandés. Que vous soyez débutant ou
+                    avancé, nos formations en Outlook, PowerPoint et Photoshop vous aideront à exceller.
                 </p>
             </div>
             <div class="px-6 pt-4 pb-2">
@@ -83,7 +88,9 @@ de Lorraine..
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Santé et Sécurité</div>
                 <p class="text-gray-700 text-base">
-                Acquérez des compétences vitales en matière de santé et de sécurité avec nos cours de premiers secours et de bonnes pratiques en sécurité. Devenez un acteur clé dans la prévention et la gestion des urgences.
+                    Acquérez des compétences vitales en matière de santé et de sécurité avec nos cours de premiers
+                    secours et de bonnes pratiques en sécurité. Devenez un acteur clé dans la prévention et la gestion
+                    des urgences.
                 </p>
             </div>
             <div class="px-6 pt-4 pb-2">
@@ -92,17 +99,36 @@ de Lorraine..
                 <span
                     class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Securité</span>
                 <span
-                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#secours </span>
+                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#secours
+                </span>
             </div>
         </div>
-    
 
-    
+        <!-- partie info supp-->
+        <div class="max-w-3xl mx-auto p-8">
+
+            <h1 class="text-3xl font-bold text-center mb-6">
+                Informations supplémentaires :
+            </h1>
+
+            <p class="text-gray-700 leading-relaxed text-center">
+                Notre catalogue de formations s'étend bien au-delà des domaines de la gestion, de l'informatique, du
+                développement durable, du secourisme et de la communication. Nous offrons également des sessions
+                spécialisées sur des sujets comme la législation sportive, l'éthique, la comptabilité associative, et
+                l'usage de logiciels spécifiques pour la gestion des clubs et des compétitions sportives. Ces formations
+                sont conçues pour répondre aux besoins des salariés et bénévoles des associations affiliées à la maison
+                régionale des sports de Lorraine. Explorez notre catalogue complet pour découvrir toutes les
+                opportunités d'apprentissage que nous proposons et choisissez celles qui correspondent le mieux à vos
+                intérêts et à vos besoins professionnels.
+            </p>
+
+        </div>
 
 
-        
 
-    
+
+
+
 
 </body>
 
