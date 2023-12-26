4c93c23e7002135f1bfb26fa13746d9d08773e3b
kckmdev <kamalichchou0@gmail.com>
2023-12-26 12:46:39 +0100

    Refactor session details display in profil.blade.php

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -39,23 +39,42 @@
                             <div class="text-md text-gray-600 mb-4">
                                 @forelse($inscriptions as $inscription)
                                 <!-- Display session details in a flex container -->
-                                <div class="flex justify-center items-center gap-4 mb-2">
-                                    <div class="bg-blue-100 text-blue-800 px-3 py-1 rounded-full">
-                                        <strong>Formation:</strong> {{$inscription->session->formation->intitule}}
-                                    </div>
-                                    <div class="bg-green-100 text-green-800 px-3 py-1 rounded-full">
-                                        <strong>Date:</strong> {{$inscription->session->date}} <!-- Assuming you have a 'date' field -->
-                                    </div>
-                                    <div class="bg-yellow-100 text-yellow-800 px-3 py-1 rounded-full">
-                                        <strong>Coût:</strong> {{$inscription->session->formation->cout}} € <!-- Assuming you have a 'prix' field -->
-                                    </div>
-                                </div>
+                                <div class="bg-white p-4 rounded-lg shadow-md mt-6">
 
+                                    <h3 class="text-lg font-medium mb-4"> {{ $inscription->session->formation->intitule }}</h3>
+
+                                    <div class="grid grid-cols-2 gap-4 mb-4">
+                                        <div>
+                                            <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                                            </svg>
+                                            <strong>Date :</strong>
+                                            <span>{{ $inscription->session->date }}</span>
+                                        </div>
+                                        <div>
+                                            <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                            </svg>
+                                            <strong>Coût :</strong>
+                                            <span>{{ $inscription->session->formation->cout }} €</span>
+                                        </div>
+                                        <div>
+                                            <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 20v-7m0 0l-9-5 9 5 9-5-9 5z"></path>
+                                            </svg>
+                                            <strong>Etat du paiement :</strong>
+                                            <span>{{ $inscription->etat_paiement }}</span>
+                                        </div>
+                                    </div>
+                                    <button class="bg-red-500 text-white px-3 py-1 rounded-md">
+                                        Annuler l'inscription
+                                    </button>
+                                </div>
                                 @empty
                                 <p class="text-center">Vous n'êtes inscrit à aucune formation.</p>
                                 @endforelse
                             </div>
-
                         </div>
                     </div>
                 </div>
